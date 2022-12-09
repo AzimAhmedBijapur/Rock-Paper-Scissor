30f2e8df39428ec4b60d30f899b3e8bd97c878c0
AzimAhmedBijapur <azeembijapur786@gmail.com>
2022-12-09 13:13:13 +0530

    code made clean; added comments ; pointerEvents on reset set to auto

--- script.js
+++ script.js
@@ -14,52 +14,89 @@ function playRound(playerChoice,computerChoice){
 
         if(playerChoice=='rock' && computerChoice=='paper')
         {
-            document.getElementById('status').style.color='red';
-            document.getElementById('status').textContent='You lose! Paper beats Rock';
+            document.getElementById('status').
+            style.color='red';
+
+            document.getElementById('status').
+            textContent='You lose! Paper beats Rock';
+
             return -1;
         }
+
         else if(playerChoice=='paper' && computerChoice=='scissor')
         {
-            document.getElementById('status').style.color='red';
-            document.getElementById('status').textContent='You lose! Scissor beats Paper';
+            document.getElementById('status')
+            .style.color='red';
+
+            document.getElementById('status')
+            .textContent='You lose! Scissor beats Paper';
+
             return -1;
         }
+
         else if(playerChoice=='scissor' && computerChoice=='rock')
         {
-            document.getElementById('status').style.color='red';
-            document.getElementById('status').textContent='You lose! Rock beats Scissor';
+            document.getElementById('status')
+            .style.color='red';
+
+            document.getElementById('status')
+            .textContent='You lose! Rock beats Scissor';
+
             return -1;
         }
+
         else if(playerChoice=='paper' && computerChoice=='rock')
         {
-            document.getElementById('status').style.color='green';
-            document.getElementById('status').textContent='You win! Paper beats Rock';
+            document.getElementById('status')
+            .style.color='green';
+
+            document.getElementById('status')
+            .textContent='You win! Paper beats Rock';
+
             return 1;
         }
+
         else if(playerChoice=='scissor' && computerChoice=='paper')
         {
-            document.getElementById('status').style.color='green';
-            document.getElementById('status').textContent='You win! Scissor beats Paper';
+            document.getElementById('status')
+            .style.color='green';
+
+            document.getElementById('status')
+            .textContent='You win! Scissor beats Paper';
+
             return 1;
         }
+
         else if(playerChoice=='rock' && computerChoice=='scissor')
         {
-            document.getElementById('status').style.color='green';
-            document.getElementById('status').textContent='You win! Rock beats Scissor';
+            document.getElementById('status')
+            .style.color='green';
+
+            document.getElementById('status')
+            .textContent='You win! Rock beats Scissor';
+
             return 1;
         }
+
         else{
-            document.getElementById('status').style.color='#bdeb05';
-            var msg= playerChoice+' vs '+computerChoice+" It's a draw !";
-            document.getElementById('status').textContent=msg;
+            document.getElementById('status').
+            style.color='#bdeb05';
+
+            var msg= playerChoice+' vs '
+            +computerChoice+" It's a draw !";
+
+            document.getElementById('status')
+            .textContent=msg;
+
             return 2;
         }
     
 }
 
-var count=0;
-var computer=0;
-var player=0;
+//Initialize scores to 0
+
+var computerScore=0;
+var playerScore=0;
 
 function game(choice){
 
@@ -69,62 +106,114 @@ function game(choice){
 
     var winOrLose=playRound(playerChoice,computerChoice);
 
-    if(winOrLose==-1){
-
-            computer+=1;
-    }
-    else if(winOrLose==1){
-            player+=1;
-    }
-    else {
-
-    }
-
-    count++;
-    
-    document.getElementById('resultOfPlayer').textContent='Player : '+player;
-    document.getElementById('resultOfComputer').textContent='Computer : '+computer;
-    
-    if(player==5 || computer ==5)
+    if(winOrLose==-1)
     {
-        document.getElementById('resultOfPlayer').textContent='Player : '+player;
-        document.getElementById('resultOfComputer').textContent='Computer : '+computer;
-        document.getElementById('rock').style.pointerEvents = 'none'; 
-        document.getElementById('paper').style.pointerEvents = 'none'; 
-        document.getElementById('scissor').style.pointerEvents = 'none'; 
 
-        if(player>computer)
+            computerScore+=1;
+    }
+
+    else if(winOrLose==1){
+            playerScore+=1;
+    }
+
+    else {
+        //Draw
+    }
+
+    //Update the score on UI
+
+    document.getElementById('resultOfPlayer').
+    textContent='Player : '+playerScore;
+
+    document.getElementById('resultOfComputer').
+    textContent='Computer : '+computerScore;
+    
+    //The first to score 5 points is winner
+
+    if(playerScore==5 || computerScore ==5)
+    {
+        document.getElementById('resultOfPlayer').
+        textContent='Player : '+playerScore;
+
+        document.getElementById('resultOfComputer').
+        textContent='Computer : '+computerScore;
+
+        //divs to select an option is locked here until reset is pressed
+
+        document.getElementById('rock').
+        style.pointerEvents = 'none'; 
+
+        document.getElementById('paper').
+        style.pointerEvents = 'none';
+
+        document.getElementById('scissor').
+        style.pointerEvents = 'none'; 
+
+        if(playerScore>computerScore)
         {
-            document.getElementById('status').style.color='green';
-            document.getElementById('status').textContent="*** You win! ***";
-            count==0;
-            player=0;
-            computer=0;
+            document.getElementById('status').
+            style.color='green';
+
+            document.getElementById('status').
+            textContent="*** You win! ***";
+
+            playerScore=0;
+            computerScore=0;
         }
-        else if(player<computer){
-            document.getElementById('status').style.color='red';
-            document.getElementById('status').textContent="*** You lose! ***";
-            count==0;
-            player=0;
-            computer=0;
+
+        else if(playerScore<computerScore){
+
+            document.getElementById('status').
+            style.color='red';
+
+            document.getElementById('status').
+            textContent="*** You lose! ***";
+
+            playerScore=0;
+            computerScore=0;
         }
+
         else{
-            document.getElementById('status').textContent="*** It's a draw! ***";
+
+            document.getElementById('status').
+            textContent="*** It's a draw! ***";
+
             count==0;
-            player=0;
-            computer=0;
+            playerScore=0;
+            computerScore=0;
         }
 
     }
 }
 
 function reset(){
-    player=0;
-    computer=0;
-    count=0;
-    document.getElementById('resultOfPlayer').textContent='Player : '+player;
-    document.getElementById('resultOfComputer').textContent='Computer : '+computer;
-    document.getElementById('status').style.color='#bdeb05';
-    document.getElementById('status').textContent="Let's begin !";
+
+    playerScore=0;
+    computerScore=0;
+    
+    //Reset score to 0 
+
+    document.getElementById('resultOfPlayer').
+    textContent='Player : '+playerScore;
+
+    document.getElementById('resultOfComputer').
+    textContent='Computer : '+computerScore;
+
+    document.getElementById('status')
+    .style.color='#bdeb05';
+    
+    document.getElementById('status').
+    textContent="Let's begin !";
+
+    //unlocking the div to select option
+
+    document.getElementById('rock').
+    style.pointerEvents ='auto' ; 
+
+    document.getElementById('paper').
+    style.pointerEvents = 'auto';
+
+    document.getElementById('scissor').
+    style.pointerEvents = 'auto';
     
 }
